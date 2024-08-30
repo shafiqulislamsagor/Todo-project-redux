af7ce4c7f52131670ce4e411422bcdf52891bb5b
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-08-30 10:50:52 +0600

    end setup and prctice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,2 +1,9 @@
-// redux-reducer
-// redux-reducer
+import { createStore } from "redux";
+import rootReducer from "./rootReducer";
+
+const store = createStore(rootReducer)
+
+
+export default store;
+
+
